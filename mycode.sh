cb481528965cec9b5558a53fb71be71e37ea59e9
jarmd <150005724+jarmd@users.noreply.github.com>
2024-04-04 13:48:28 +0200

    fix(code): this is with 2 breaking changes in commit msg (#73)
    BREAKING CHANGE: This is my breaking body
    
    BREAKING CHANGE: Footer breaking change.

--- mycode.sh
+++ mycode.sh
@@ -2,4 +2,5 @@
 echo "Hello evil nicklas"
 echo "We try to make nicklas good"
 echo "Adding 1 new line i commit -m message after subject - major test-7"
+echo "Adding 1 new line a body and then a blank and then footer BREAKING CHANGE in commit -m message after subject - major test-8"
 
