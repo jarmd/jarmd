eaa4621872b4199e3c2798cb4b23a012445e6f6d
jarmd <150005724+jarmd@users.noreply.github.com>
2024-04-04 13:59:23 +0200

    fix(code): adding 2 blanks before breaking (#74)
    BREAKING CHANGE: This would upgrade MAJOR 2 blanks before this.

--- mycode.sh
+++ mycode.sh
@@ -3,4 +3,5 @@ echo "Hello evil nicklas"
 echo "We try to make nicklas good"
 echo "Adding 1 new line i commit -m message after subject - major test-7"
 echo "Adding 1 new line a body and then a blank and then footer BREAKING CHANGE in commit -m message after subject - major test-8"
+echo "8th worked fine and both breaking was taken but footer triggered MAJOR version - now with 2 blanks before footer"
 
