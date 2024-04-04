39cdf6b6e59e38d9caaa27824ff60e9987f4ff62
Jan Primdahl Madsen <jarmd@vestas.com>
2024-04-04 14:28:40 +0200

    fix(mycode.sh): this will do a major upgrade to 3.0

--- mycode.sh
+++ mycode.sh
@@ -4,4 +4,4 @@ echo "We try to make nicklas good"
 echo "Adding 1 new line i commit -m message after subject - major test-7"
 echo "Adding 1 new line a body and then a blank and then footer BREAKING CHANGE in commit -m message after subject - major test-8"
 echo "8th worked fine and both breaking was taken but footer triggered MAJOR version - now with 2 blanks before footer"
-
+echo "This will upgrade our version to version 3.0 using only GitHub as our submitter"
